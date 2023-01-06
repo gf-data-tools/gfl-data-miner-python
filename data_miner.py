a90140b3e03f306ac49914d80585f509b96cb0a4
ZeroRin <pptxp@qq.com>
2023-01-06 10:50:11 +0800

    raise error at the end if any run fail

--- data_miner.py
+++ data_miner.py
@@ -273,6 +273,7 @@ if __name__=='__main__':
     parser.add_argument('--force', '-f',action='store_true')
     parser.add_argument('--loglevel',default='INFO',choices=['DEBUG','INFO','WARNING','ERROR','CRITICAL'])
     args=parser.parse_args()
+    error = False
     for region in args.region:
         print(f"::group::{region.upper()} Server")
         try:
@@ -282,5 +283,8 @@ if __name__=='__main__':
         except Exception as e:
             logging.error(traceback.format_exc())
             logging.error(f"Extraction failed due to {e}")
+            error = True
         print("::endgroup::")
+    if error:
+        raise RuntimeError('Error during execution')
 # %%
